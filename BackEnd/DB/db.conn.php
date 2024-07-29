641d46b483ba77fb4981a3e1bc006e099b0695c8
Sachin Gunasekara <gunasekara.s.u.d.a@gmail.com>
2024-07-29 15:36:49 +0530

    create db connection

--- BackEnd/DB/db.conn.php
+++ BackEnd/DB/db.conn.php
@@ -1,4 +1,18 @@
 <?php 
 
+$serverName = "localhost"; // Remove 'https://' and any protocol prefixes
+$userName = "demo";
+$password = "iRoDUi8QoZgQIYs";
+$dbName = "ea_web_admin";
+
+try {
+    $conn = new PDO("mysql:host=$serverName;dbname=$dbName", $userName, $password);
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "Connection Failed: " . $e->getMessage();
+    exit; // Stop execution if connection fails
+}
+
 
 ?>
